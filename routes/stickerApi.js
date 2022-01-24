f206b476a313868f91601b952f25821d060515c8
leecheng0115 <best.dev@outlook.com>
2022-01-24 09:21:45 +0900

    fix:remove getsoldcollectible api endpoint

--- routes/stickerApi.js
+++ routes/stickerApi.js
@@ -585,40 +585,6 @@ router.get('/getBoughtNotSoldCollectible', function(req, res) {
     })
 })
 
-router.get('/getSoldCollectible', function(req, res) {
-    let selfAddr = req.query.selfAddr;
-    let pageNumStr = req.query.pageNum;
-    let pageSizeStr = req.query.pageSize;
-    let keyword = req.query.keyword;
-    let orderType = req.query.orderType;
-    let filter_status = req.query.filter_status;
-    let filter_min_price = req.query.filter_status;
-    let filter_max_price = req.query.filter_min_price;
-    let pageNum, pageSize;
-
-    try {
-        pageNum = pageNumStr ? parseInt(pageNumStr) : 1;
-        pageSize = pageSizeStr ? parseInt(pageSizeStr) : 10;
-        keyword = keyword ? keyword : '';
-        filter_status = filter_status ? filter_status : '';
-        filter_min_price = filter_min_price ? filter_min_price : 0;
-        filter_max_price = filter_max_price ? filter_max_price : 99999999999999999999999999;
-        if(pageNum < 1 || pageSize < 1) {
-            res.json({code: 400, message: 'bad request'})
-            return;
-        }
-    }catch (e) {
-        console.log(e);
-        res.json({code: 400, message: 'bad request'});
-        return;
-    }
-
-    stickerDBService.getSoldCollectible(pageNum, pageSize, keyword, orderType, filter_status, filter_min_price, filter_max_price, selfAddr).then(result => {
-        res.json(result);
-    }).catch(error => {
-        res.json({code: 500, message: 'server error'});
-    })
-});
 
 router.get('/getOwnCollectible', function(req, res) {
     let selfAddr = req.query.selfAddr;
