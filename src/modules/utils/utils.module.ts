e86d22a0d86a998259275374c364f1b51f85f577
LiFayi <lifayi2008@gmail.com>
2022-07-14 15:11:11 +0800

    add email notifications

--- src/modules/utils/utils.module.ts
+++ src/modules/utils/utils.module.ts
@@ -2,10 +2,11 @@ import { Web3Service } from './web3.service';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { UtilsService } from './utils.service';
+import { MailService } from './mail.service';
 
 @Module({
   imports: [ConfigModule],
-  providers: [Web3Service, UtilsService],
-  exports: [Web3Service, UtilsService],
+  providers: [Web3Service, UtilsService, MailService],
+  exports: [Web3Service, UtilsService, MailService],
 })
 export class UtilsModule {}
